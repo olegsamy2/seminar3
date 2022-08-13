9358f943f84660db09709ddb487dcf2f01ac315f
OlegSam2 <doorsmania@mail.ru>
2022-08-13 11:24:04 +0300

    разбор ДЗ

---
+++ taskDZ/Program.cs
@@ -0,0 +1,25 @@
+﻿// int a = 0;
+// char aa = '0';
+// Console.WriteLine(a);
+// Console.WriteLine((int)aa); // char 0 = int 48 (табл Ascii)
+
+//  int b = aa - '0';  // b=48-48 =0
+
+// // Console.WriteLine((int)aa);
+// Console.WriteLine(b);
+
+// // ascii  52-48 task13
+
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number>=100 & number<1000)
+// {
+    //int digit2 = (number % 100)/10;
+    int digit2 = (782 % 100)/10;
+    Console.WriteLine($"Вторая цифра этого числа - {digit2}");
+//     Console.WriteLine($"Вторая цифра этого числа - {digit2}");
+// }
+// else
+// {
+//     Console.WriteLine("Error. Введите трёхзначное число.");
+// }
