e5ff4300f9bc7034e0cebd3f3f674936f92cd158
OlegSam2 <doorsmania@mail.ru>
2022-08-13 12:44:56 +0300

    обратное task17

---
+++ task18/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 18: Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y)
+// обратная задаче 17
+Console.WriteLine("Введите номер четверти координат осьей ХУ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1)
+{
+    Console.WriteLine("X > 0 и Y > 0");
+}
+else if (number == 2)
+{
+    Console.WriteLine("X < 0 и Y > 0");
+}
+else if (number == 3)
+{       
+     Console.WriteLine("X < 0 и Y < 0");
+}
+else if (number == 4)
+{   Console.WriteLine("X > 0 и Y < 0");
+}
+
+else
+{
+    Console.WriteLine("Введен неправильный номер");
+}
